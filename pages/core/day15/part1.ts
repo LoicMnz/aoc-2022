fc13ad4f7a175e29ff98d393c2a0e08a02c36320
Loïc Monsorez <62388938+LoicMnz@users.noreply.github.com>
2022-12-15 13:07:50 +0100

    Jour 15

--- pages/core/day15/part1.ts
+++ pages/core/day15/part1.ts
@@ -1,3 +1,51 @@
+import { pointSchema, pointType } from "core/day14/cave.model";
+import { reduce } from "core/util/array";
+import { RETOUR_LIGNE } from "core/util/constantes";
+import { parseNumber } from "core/util/type";
 import { input } from "./input";
+const nextValue = (s: string, sep: string) => {
+  return s.split(sep)[0];
+};
+const distance = (a: pointType, b: pointType) => {
+  return Math.abs(a[0] - b[0]) + Math.abs(a[1] - b[1]);
+};
+const serialize = (point: pointType) => {
+  return point[0] + " " + point[1];
+};
+type Sensor = pointType;
+type Beacon = pointType;
+type Detect = { s: Sensor; b: Beacon; d: number };
+const sensors: Detect[] = [];
+input.split(RETOUR_LIGNE).map((line) => {
+  const tab = line.split("=");
+  const s: Sensor = [
+    parseNumber(nextValue(tab[1], ",")),
+    parseNumber(nextValue(tab[2], ":")),
+  ];
+  const b: Beacon = [parseNumber(nextValue(tab[3], ",")), parseNumber(tab[4])];
+  const d = distance(s, b);
+  const det = { s, b, d };
+  sensors.push(det);
+});
 
-export default true;
+const minX = sensors.map((e) => e.s[0] - e.d).reduce(reduce.min);
+const maxX = sensors.map((e) => e.s[0] + e.d).reduce(reduce.max);
+const y = 2000000;
+// const y = 10;
+const liste = new Set<string>();
+
+for (let index = minX; index < maxX + 1; index++) {
+  const point = pointSchema.parse([index, y]);
+  for (const det of sensors) {
+    if (distance(det.s, point) <= det.d) {
+      liste.add(serialize(point));
+      break;
+    }
+  }
+}
+for (const det of sensors) {
+  liste.delete(serialize(det.b));
+  liste.delete(serialize(det.s));
+}
+
+export default liste.size;
--- pages/core/day15/part2.ts
+++ pages/core/day15/part2.ts
@@ -1,3 +1,75 @@
+import { pointType } from "core/day14/cave.model";
+import { RETOUR_LIGNE } from "core/util/constantes";
+import { parseNumber } from "core/util/type";
+import { Ranges, rMerge } from "ranges-merge";
+import { assert } from "tsafe";
 import { input } from "./input";
 
-export default true;
+const max = 4000000;
+const nextVal = (s: string, sep: string) => {
+  return s.split(sep)[0];
+};
+const distance = (a: pointType, b: pointType) => {
+  return Math.abs(a[0] - b[0]) + Math.abs(a[1] - b[1]);
+};
+
+const intersectionLigne = (y: number, d: Detect) => {
+  if (d.s[1] + d.d >= y && y >= d.s[1] - d.d) {
+    return true;
+  }
+  return false;
+};
+const getRange = (d: Detect, y: number) => {
+  // const result = [d.s[0] - d.d - d.s[1] + y, d.s[0] + d.d + d.s[1] - y];
+  const distanceSource = Math.abs(d.s[1] - y);
+  const result = [d.s[0] - d.d + distanceSource, d.s[0] + d.d - distanceSource];
+
+  if (result[0] < 0) {
+    result[0] = 0;
+  }
+  return result;
+};
+
+const parcoursLigne = (sensors: Detect[], y: number): false | Ranges => {
+  const dets = sensors.filter((det) => intersectionLigne(y, det));
+  const merge = rMerge(
+    //@ts-ignore
+    dets.map((d) => getRange(d, y))
+  );
+
+  return merge?.length !== 1 && merge;
+};
+type Sensor = pointType;
+type Beacon = pointType;
+type Detect = { s: Sensor; b: Beacon; d: number };
+const sensors: Detect[] = [];
+input.split(RETOUR_LIGNE).map((line) => {
+  const tab = line.split("=");
+  const s: Sensor = [
+    parseNumber(nextVal(tab[1], ",")),
+    parseNumber(nextVal(tab[2], ":")),
+  ];
+  const b: Beacon = [parseNumber(nextVal(tab[3], ",")), parseNumber(tab[4])];
+  const d = distance(s, b);
+  const det = { s, b, d };
+  sensors.push(det);
+});
+
+let result = -1;
+// const y = 10;
+for (let y = 0; y < max; y++) {
+  const ranges = parcoursLigne(sensors, y);
+  if (ranges) {
+    const a = ranges.pop();
+    const b = ranges.pop();
+    assert(a !== undefined && b !== undefined);
+    if (b[1] + 1 !== a[0]) {
+      const x = b[1] + 1;
+      result = x * 4000000 + y;
+    }
+  }
+}
+
+export default result;
+
+// 10649099160102 too low
