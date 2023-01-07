4f3fa160ee0a9a483ef8a29d81e5ae042413629b
Loïc M <62388938+LoicMnz@users.noreply.github.com>
2023-01-07 11:54:45 +0100

    Jour 18 part 2

--- pages/core/day18/Drop.ts
+++ pages/core/day18/Drop.ts
@@ -10,4 +10,14 @@ export class Drop {
   toString() {
     return `${this.x},${this.y},${this.z}`;
   }
+  findAdjacent() {
+    const adjacents: Drop[] = [];
+    adjacents.push(new Drop(this.x + 1, this.y, this.z));
+    adjacents.push(new Drop(this.x - 1, this.y, this.z));
+    adjacents.push(new Drop(this.x, this.y + 1, this.z));
+    adjacents.push(new Drop(this.x, this.y - 1, this.z));
+    adjacents.push(new Drop(this.x, this.y, this.z + 1));
+    adjacents.push(new Drop(this.x, this.y, this.z - 1));
+    return adjacents;
+  }
 }
--- pages/core/day18/part2.ts
+++ pages/core/day18/part2.ts
@@ -1,114 +1,74 @@
+import { reduce } from "core/util/array";
 import { RETOUR_LIGNE } from "core/util/constantes";
 import { parseNumber } from "core/util/type";
 import { Drop } from "./Drop";
 import { input, inputEx } from "./input";
-const inp = inputEx;
+const inp = input;
 const drops = inp
   .split(RETOUR_LIGNE)
   .map((l) => l.split(",").map((i) => parseNumber(i)))
   .map((l) => new Drop(l[0], l[1], l[2]));
+const dropsString = drops.map((e) => e.toString());
+const next: Drop[] = [new Drop(0, 0, 0)];
+const visited: Map<string, number> = new Map();
 
-let surfaceArea = drops.length * 6;
-const isContinueDrop = (drop1: Drop, drop2: Drop) => {
-  return (
-    (drop1.x === drop2.x &&
-      drop1.y === drop2.y &&
-      (drop1.z === drop2.z + 1 || drop1.z === drop2.z - 1)) ||
-    (drop1.x === drop2.x &&
-      (drop1.y === drop2.y - 1 || drop1.y === drop2.y + 1) &&
-      drop1.z === drop2.z) ||
-    ((drop1.x === drop2.x - 1 || drop1.x == drop2.x + 1) &&
-      drop1.y === drop2.y &&
-      drop1.z === drop2.z)
-  );
+const minX = drops.map((e) => e.x).reduce(reduce.min);
+const maxX = drops.map((e) => e.x).reduce(reduce.max);
+const minY = drops.map((e) => e.y).reduce(reduce.min);
+const maxY = drops.map((e) => e.y).reduce(reduce.max);
+const minZ = drops.map((e) => e.z).reduce(reduce.min);
+const maxZ = drops.map((e) => e.z).reduce(reduce.max);
+const inGrid = (point: Drop) => {
+  const { x, y, z } = point;
+  if (minX - 1 > x || x > maxX + 1) {
+    return false;
+  }
+  if (minY - 1 > y || y > maxY + 1) {
+    return false;
+  }
+  if (minZ - 1 > z || z > maxZ + 1) {
+    return false;
+  }
+  return true;
 };
-for (let i = 0; i < drops.length; i++) {
-  const e1 = drops[i];
-  for (let j = i + 1; j < drops.length; j++) {
-    const e2 = drops[j];
-    if (isContinueDrop(e1, e2)) {
-      surfaceArea -= 2;
-    }
+let drop: Drop;
+while (next.length !== 0) {
+  const drop = next.pop()!;
+  const dropString = drop.toString();
+  if (!inGrid(drop)) {
+    console.log(drop);
+    continue;
   }
-}
-const dropStrings = drops.map((d) => [d.x, d.y, d.z].join(","));
-const findBubbleUp = (drop: Drop, drops: Drop[]) => {
-  let count = 0;
-  for (let index = 0; index < drops.length; index++) {
-    const element = drops[index];
-    if (dropStrings.includes([drop.x, drop.y, drop.z + 1].join(","))) {
-      break;
+  if (!dropsString.includes(dropString) && !visited.has(dropString)) {
+    // how many drop neighbor
+    let count = 0;
+    for (let j = 0; j < drops.length; j++) {
+      const e2 = drops[j];
+      if (
+        (drop.x === e2.x &&
+          drop.y === e2.y &&
+          (drop.z === e2.z + 1 || drop.z === e2.z - 1)) ||
+        (drop.x === e2.x &&
+          (drop.y === e2.y - 1 || drop.y === e2.y + 1) &&
+          drop.z === e2.z) ||
+        ((drop.x === e2.x - 1 || drop.x == e2.x + 1) &&
+          drop.y === e2.y &&
+          drop.z === e2.z)
+      ) {
+        count++;
+      }
     }
-    if (
-      (element.x === drop.x &&
-        element.y === drop.y &&
-        element.z === drop.z + 2) ||
-      ((element.x === drop.x - 1 || element.x === drop.x + 1) &&
-        element.y === drop.y &&
-        element.z === drop.z + 1) ||
-      ((element.y === drop.y - 1 || element.y === drop.y + 1) &&
-        element.x === drop.x &&
-        element.z === drop.z + 1)
-    ) {
-      count++;
-    }
-  }
-  if (count === 5) {
-    const index = drops.findIndex((e) => e === drop);
-    console.log(index, "one drop", drop);
-    return new Drop(drop.x, drop.y, drop.z + 1);
-  }
-};
-// 4120 too high
+    visited.set(dropString, count);
 
-const bubbles = drops
-  .map((d) => findBubbleUp(d, drops))
-  .filter((e) => e !== undefined) as Drop[];
-
-let continueBubble = 0;
-for (let i = 0; i < bubbles.length; i++) {
-  const e1 = bubbles[i];
-  for (let j = i + 1; j < bubbles.length; j++) {
-    const e2 = bubbles[j];
-    if (isContinueDrop(e1, e2)) {
-      continueBubble++;
-    }
+    // next step
+    drop.findAdjacent().forEach((e) => next.push(e));
   }
 }
 
-console.log("number drops", drops.length);
-console.log("initial surface", drops.length * 6);
-console.log("surfaceWithContinueDrop", surfaceArea);
-console.log("number bubble", bubbles.length);
-console.log("number continue bubble", continueBubble);
+console.log(visited);
+let result = 0;
+visited.forEach((v, k) => {
+  result += v;
+});
 
-console.log(
-  "x",
-  drops.reduce((c, d) => Math.min(c, d.x), Infinity),
-  drops.reduce((c, d) => Math.max(c, d.x), 0),
-  drops.filter((e) => e.x === 1).map((e) => e.y),
-  drops.filter((e) => e.x === 1).map((e) => e.z),
-  drops.filter((e) => e.x === 3).map((e) => e.y),
-  drops.filter((e) => e.x === 3).map((e) => e.z)
-);
-console.log(
-  "y",
-  drops.reduce((c, d) => Math.min(c, d.y), Infinity),
-  drops.reduce((c, d) => Math.max(c, d.y), 0),
-  drops.filter((e) => e.y === 1).map((e) => e.x),
-  drops.filter((e) => e.y === 1).map((e) => e.z),
-  drops.filter((e) => e.y === 3).map((e) => e.x),
-  drops.filter((e) => e.y === 3).map((e) => e.z)
-);
-console.log(
-  "z",
-  drops.reduce((c, d) => Math.min(c, d.z), Infinity),
-  drops.reduce((c, d) => Math.max(c, d.z), 0),
-  drops.filter((e) => e.z === 1).map((e) => e.x),
-  drops.filter((e) => e.z === 1).map((e) => e.y),
-  drops.filter((e) => e.z === 6).map((e) => e.x),
-  drops.filter((e) => e.z === 6).map((e) => e.y)
-);
-surfaceArea =
-  surfaceArea - continueBubble * 5 - (bubbles.length - continueBubble) * 6;
-export default surfaceArea;
+export default result;
